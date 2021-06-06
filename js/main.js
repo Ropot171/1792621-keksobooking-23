7fd22e503eaf6fc239c645877edde1bffd0b10cb
Мила <l.milaropot@gmail.com>
2021-06-06 22:59:32 +0300

    Исправление ошибок 3

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ function getRandomNumber(min, max) {
   const minNumber = Math.ceil(min);
   const maxNumber = Math.floor(max);
   if ( maxNumber < minNumber){
-    throw new RangeError('Максимальное значение числа не должно быть равно минимальному значению числа. Результат + min');
+    throw new RangeError('Значение максильного числа не должно быть меньше значения минимального числа');
   } else if (maxNumber === minNumber) {
-    console.warn(min); //можно вернуть minNumber, но этим мы можем запутать пользователя
+    throw new RangeError('Максимальное значение числа не должно быть равно минимальному значению числа. Результат + min');
   } else {
     return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-  }
+}
 }
 getRandomNumber(500, 600);
 function getRandomFloat(minParameter, maxParameter, quantity) {
@@ -18,6 +18,6 @@ function getRandomFloat(minParameter, maxParameter, quantity) {
   } else {
     const result =  Math.random() * (maxParameter - minParameter) + minParameter;
     return Number(result.toFixed(quantity)); 
-  }
+}
 }
 getRandomFloat(135.38490, 138.32789, 3);
