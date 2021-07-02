56f48342d2a62c85467736ff8bc5e022d4026430
Мила <l.milaropot@gmail.com>
2021-07-02 14:19:40 +0300

    Fix v3

--- js/page.js
+++ js/page.js
@@ -1,10 +1,10 @@
 const adForm = document.querySelector('.ad-form');
 const formFilter = document.querySelector('.map__filters');
 
-function disableTheForm(isDisabled) {
+function changeFormState(state) {
   [adForm, formFilter].forEach((form) => {
     for (const element of form.elements) {
-      element.disabled = isDisabled;
+      element.disabled = state;
     }
   });
 }
@@ -12,7 +12,7 @@ function disableTheForm(isDisabled) {
 function changePageState(isDisabled) {
   adForm.classList.toggle('ad-form--disabled', isDisabled);
   formFilter.classList.toggle('ad-form--disabled', isDisabled);
-  disableTheForm(isDisabled);
+  changeFormState(isDisabled);
 }
 
 export {changePageState};
